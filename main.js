ce5d232bdbd38db0c62b9bb4d9386a7b98a9b2ee
cfz6677 <cfz6677@gmail.com>
2021-11-09 16:07:00 -0700

    changes1

--- main.js
+++ main.js
@@ -48,10 +48,15 @@ class Ball {
                 var ctx = canvas.getContext('2d');
 
                 ctx.fillStyle = 'rgb(200, 0, 0)';
-                ctx.fillArc(10, 10, 50, 50);
+                ctx.fillRect(10, 10, 50, 50);
 
                 ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-                ctx.fillArc(30, 30, 50, 50);
+                ctx.fillRect(30, 30, 50, 50);
+
+                var x = canvas.width / 2;
+                var y = canvas.height - 10;
+                var dx = 2;
+                var dy = -2;
 
 
 
@@ -62,50 +67,65 @@ class Ball {
         }
     }
 }
-        var ball1 = new Ball(100, 100, "white", 100);
-        var ball2 = new Ball(50, 50, "aqua", 10);
-        var ball3 = new Ball(50, 50, "aquamarine", 10);
-        var ball4 = new Ball(50, 50, "azure", 10);
-        var ball5 = new Ball(50, 50, "black", 10);
-        var ball6 = new Ball(50, 50, "blue", 10);
-        var ball7 = new Ball(50, 50, "blueviolet", 10);
-        var ball8 = new Ball(50, 50, "charteuse", 10);
-        var ball9 = new Ball(50, 50, "coral", 10);
-        var ball10 = new Ball(50, 50, "cornflowerblue", 10);
-        var ball11 = new Ball(50, 50, "crimson", 10);
-        var ball12 = new Ball(50, 50, "cyan", 10);
-        var ball13 = new Ball(50, 50, "darkgreen", 10);
-        var ball14 = new Ball(50, 50, "darkmagenta", 10);
-        var ball15 = new Ball(50, 50, "gold", 10);
-        var ball16 = new Ball(50, 50, "gray", 10);
-        var ball17 = new Ball(50, 50, "green", 10);
-        var ball18 = new Ball(50, 50, "greenyellow", 10);
-        var ball19 = new Ball(50, 50, "honeydew", 10);
-        var ball20 = new Ball(50, 50, "hotpink", 10);
-        var ball21 = new Ball(50, 50, "indianred", 10);
-        var ball22 = new Ball(50, 50, "indigo", 10);
-        var ball23 = new Ball(50, 50, "lavender", 10);
-        var ball24 = new Ball(50, 50, "lightblue", 10);
-        var ball25 = new Ball(50, 50, "mediumspringgreen", 10);
-        var ball26 = new Ball(50, 50, "navy", 10);
-        var ball27 = new Ball(50, 50, "olive", 10);
-        var ball28 = new Ball(50, 50, "orange", 10);
-        var ball29 = new Ball(50, 50, "palegreen", 10);
-        var ball30 = new Ball(50, 50, "purple", 10);
+var ball1 = new Ball(100, 100, "white", 100);
+var ball2 = new Ball(50, 50, "aqua", 10);
+var ball3 = new Ball(50, 50, "aquamarine", 10);
+var ball4 = new Ball(50, 50, "azure", 10);
+var ball5 = new Ball(50, 50, "black", 10);
+var ball6 = new Ball(50, 50, "blue", 10);
+var ball7 = new Ball(50, 50, "blueviolet", 10);
+var ball8 = new Ball(50, 50, "charteuse", 10);
+var ball9 = new Ball(50, 50, "coral", 10);
+var ball10 = new Ball(50, 50, "cornflowerblue", 10);
+var ball11 = new Ball(50, 50, "crimson", 10);
+var ball12 = new Ball(50, 50, "cyan", 10);
+var ball13 = new Ball(50, 50, "darkgreen", 10);
+var ball14 = new Ball(50, 50, "darkmagenta", 10);
+var ball15 = new Ball(50, 50, "gold", 10);
+var ball16 = new Ball(50, 50, "gray", 10);
+var ball17 = new Ball(50, 50, "green", 10);
+var ball18 = new Ball(50, 50, "greenyellow", 10);
+var ball19 = new Ball(50, 50, "honeydew", 10);
+var ball20 = new Ball(50, 50, "hotpink", 10);
+var ball21 = new Ball(50, 50, "indianred", 10);
+var ball22 = new Ball(50, 50, "indigo", 10);
+var ball23 = new Ball(50, 50, "lavender", 10);
+var ball24 = new Ball(50, 50, "lightblue", 10);
+var ball25 = new Ball(50, 50, "mediumspringgreen", 10);
+var ball26 = new Ball(50, 50, "navy", 10);
+var ball27 = new Ball(50, 50, "olive", 10);
+var ball28 = new Ball(50, 50, "orange", 10);
+var ball29 = new Ball(50, 50, "palegreen", 10);
+var ball30 = new Ball(50, 50, "purple", 10);
 
 
 
-        // // ======= Functions
-        // let player, ctx, balls
+// // ======= Functions
+//  player, ctx, balls
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
 
 
-        function init() {
-            // canvas.width = 400
-            // canvas.height = 400
-            // window.requestAnimationFrame(900)
-            ball1.render();
-        }
-        init();
+function init() {
+    // canvas.width = 400
+    // canvas.height = 400
+    // window.requestAnimationFrame(900)
+    ball1.render();
+}
+init();
 
 
 // //==============Objects
